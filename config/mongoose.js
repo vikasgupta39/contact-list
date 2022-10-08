58917192a25c7dd25b7462a263f75042921107e1
vikas gupta <vikasguptaamit.com>
2022-10-08 19:49:11 +0530

    some changes

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@
 const mongoose=require('mongoose');
 
 // connect the database
-mongoose.connect('mongodb://0.0.0.0:27017/contact_list_db');
+mongoose.connect('mongodb://localhost/contact_list_db');
 const db=mongoose.connection;
 // error
 db.on('error',console.error.bind(console,'error connecting to db'));
